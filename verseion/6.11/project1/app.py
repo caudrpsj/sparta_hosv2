c2c9f4bdea6fb5698e8cfbb0fd4d53881431e81f
caudrpsj <65058598+caudrpsj@users.noreply.github.com>
2020-06-11 20:19:11 +0900

    Update app.py

--- verseion/6.11/project1/app.py
+++ verseion/6.11/project1/app.py
@@ -38,15 +38,26 @@ def saving():
    #       'abstract': number_receive
    #       }
    #  db.pubmed.insert_one(article)
+  choose_receive = request.form['choose_give']
+  print(choose_receive)
+  if choose_receive == '2' :
+    print('match 함수로')
+    idlist = saving_match(search_receive,number_receive)
+  elif choose_receive == '3' :
+    print('날짜 함수로')
+    idlist = saving_date(search_receive,number_receive)
+  else :  
+    handle = Entrez.esearch(db="pubmed", term=search_receive, retmax=number_receive, sort='relevance')
+    record = Entrez.read(handle)
+    idlist = record["IdList"]
 
-  handle = Entrez.esearch(db="pubmed", term=search_receive, retmax=number_receive, sort='relevance')
-  record = Entrez.read(handle)
-  idlist = record["IdList"]
   handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline")
   records = Medline.parse(handle)
   records = list(records)
   print (search_receive)
+  num = 0
   for record in records:
+    num  += 1
     title = record.get("TI", "?")
     abstract = record.get("AB", "No abstract available")
     pmid = record.get("PMID", "")
@@ -60,10 +71,54 @@ def saving():
       'PMID' : pmid,
       'DOI' : doi,
       'journal': journal,
-      'PMC':pmc
+      'PMC':pmc,
+      'Num' : num
+
        }
     db.pubmed.insert_one(article)  
   return jsonify({'result': 'success', 'msg': '로딩 완료.'})
+
+
+# 검색함수 더만들기 
+def saving_match(search_receive,number_receive):
+  print('name')
+  headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+  request_url = 'https://pubmed.ncbi.nlm.nih.gov/?term='+search_receive+'&size=50'
+  data = requests.get(request_url,headers=headers)
+  # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+  soup = BeautifulSoup(data.text, 'html.parser')
+  #//*[@id="search-results"]/section/div[1]/div/article[1]/div[2]/div[1]/div[1]/span[3]/text()
+  # select를 이용해서, tr들을 불러오기
+  #/html/body/main/div[9]/div[2]/section/div[1]/div/article[1]/div[2]/div[1]/div[1]/span[5]/span
+  pubmeds = soup.select('#search-results > section > div.search-results-chunks > div > article.labs-full-docsum')
+  # pubmeds = soup.select('#search-results > section > div.search-results-chunks > div > article.labs-full-docsum > div.docsum-content > div.labs-docsum-citation full-citation')
+  # movies (tr들) 의 반복문을 돌리기
+  num = 0
+  idlist = list()
+  number_receive = int(number_receive)  
+  for pubmeds_in in pubmeds:
+      num  += 1
+      # movie 안에 a 가 있으면,
+      a_tag = pubmeds_in.select_one('div.docsum-wrap > div.docsum-content > div >span:nth-child(5)')
+      b_tag = a_tag.text.split(': ')[1]
+      # print (b_tag)
+      # doi_split = b_tag.split('doi:')[1].split('. ')[0]
+      print(number_receive)
+      print(num)
+      idlist.append(b_tag)
+      # if a_tag is not None:
+      #     print(a_tag.text)
+      if num == number_receive :
+        break
+      
+  return idlist
+
+def saving_date(search_receive,number_receive):
+  handle = Entrez.esearch(db="pubmed", term=search_receive, retmax=number_receive, sort='pub date')
+  print('pubdate')
+  record = Entrez.read(handle)
+  idlist = record["IdList"]
+  return idlist
 
 # 다운로드 이치
 @app.route('/download', methods=['POST'])
@@ -77,21 +132,32 @@ def downloadeach():
 
   try : 
     downloadeach_doi(downloadtarget)
+    msg = downloadeach_doi(downloadtarget)
   except:   
     print('pmc 확인하러 간다') 
-    downloadeach_pmc_yes(downloadtarget) 
+    msg = downloadeach_pmc_yes(downloadtarget) 
 
-  return jsonify({'result': 'success', 'msg': '다운르도 성공적으로 완료되었습니다.'})
+  return jsonify({'result': 'success', 'msg': msg})
 
 
 def downloadeach_doi(downloadtarget):
   downloadtarget_doi = downloadtarget['DOI']
+  
   if downloadtarget_doi.count('[') >1 :
     url = downloadtarget['DOI'].split('[doi')[0].split('] ')[1]
   else :
     url = downloadtarget['DOI'].split('[doi')[0]
-  print(downloadtarget_doi)
+  #잠시 한번더확인
+  if downloadtarget_doi == '' : 
+    print('doi에는 안들어있음')
+    downloadtarget_doi_confirm = downloadtarget['journal']
+    downloadtarget_doi = downloadtarget_doi_confirm.split('doi: ')[1].split('. ')[0]
+    print ('새로구한 doi')
+    print(downloadtarget_doi)
+    url = downloadtarget_doi
+  # 확인
   url_scihub = 'https://sci-hub.tw/' + url
+  print(url_scihub)
   headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
   data = requests.get(url_scihub,headers=headers)
   soup = BeautifulSoup(data.text, 'html.parser')
@@ -117,6 +183,7 @@ def downloadeach_doi(downloadtarget):
   open("sichub", mode="wb")
   urllib.request.urlretrieve(url_download,outpath+outfile)
   print('저장되었습니다')
+  return '다운로드 성공'
   
   # pmc_receive = downloadtarget['PMC']
   # print(pmc_receive)
@@ -137,33 +204,36 @@ def downloadeach_pmc_yes(downloadtarget) :
   pmc_receive = downloadtarget['PMC']
   if 'PMC' in pmc_receive :
     downloadeach_pmc(downloadtarget)
+    return downloadeach_pmc(downloadtarget)
 
   else : 
     print('pmc에 뭐가 없음')
+    msg = '다운로드 실패'
+    return msg
 
 def downloadeach_pmc(downloadtarget) :
   pmc_receive = downloadtarget['PMC']
   url_pmc = 'https://www.ncbi.nlm.nih.gov/pmc/articles/' + pmc_receive
   print(url_pmc)
-  headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-  data = requests.get(url_pmc,headers=headers)
-  soup = BeautifulSoup(data.text, 'html.parser')
-  url_download2 = soup.select_one('#rightcolumn>div:nth-child(2)>div>ul>li:nth-child(4)>a')
-  print(url_download2)
-  url_download = url_download2['href']
-  url_download = 'https://www.ncbi.nlm.nih.gov/' + url_download
-  print(url_download)
-  path_name = downloadtarget['search']
-  file_name = downloadtarget['PMID']
-  outpath = "C:/test/"+ path_name +"/" # 여기는 검색어로 
-  outfile = file_name+".pdf"  # 여기는 서지 정보 및 pmid
-  print(path_name)
-  print(file_name)
-  import urllib.request
-  open("sichub", mode="wb")
-  urllib.request.urlretrieve(url_download,outpath+outfile)
-  print('저장되었습니다')
-
+  # headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+  # data = requests.get(url_pmc,headers=headers)
+  # soup = BeautifulSoup(data.text, 'html.parser')
+  # url_download2 = soup.select_one('#rightcolumn>div:nth-child(2)>div>ul>li:nth-child(4)>a')
+  # print(url_download2)
+  # url_download = url_download2['href']
+  # url_download = 'https://www.ncbi.nlm.nih.gov/' + url_download
+  # print(url_download)
+  # path_name = downloadtarget['search']
+  # file_name = downloadtarget['PMID']
+  # outpath = "C:/test/"+ path_name +"/" # 여기는 검색어로 
+  # outfile = file_name+".pdf"  # 여기는 서지 정보 및 pmid
+  # print(path_name)
+  # print(file_name)
+  # import urllib.request
+  # open("sichub", mode="wb")
+  # urllib.request.urlretrieve(url_download,outpath+outfile)
+  # print('저장되었습니다')
+  return url_pmc
 
 if __name__ == '__main__':
     app.run('localhost',port=5000,debug=True)
